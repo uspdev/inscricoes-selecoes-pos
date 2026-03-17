feef2141f7ec00162aa6bf55f10538e75bb043d0
João Pedro de Toledo <jpedrotoledo@usp.br>
2026-03-17 18:13:17 -0300

    adiciona os links como anchor tag nos emails de confirmação de usuario e recuperação de senha

--- resources/views/emails/localuser_confirmacaodeemail.blade.php
+++ resources/views/emails/localuser_confirmacaodeemail.blade.php
@@ -1,7 +1,7 @@
 Olá {{ $localuser->name }},<br />
 <br />
 Você se cadastrou no sistema de inscrições para seleções da pós-graduação.<br />
-Clique neste link para confirmar seu e-mail: {{ $email_confirmation_url }}<br />
+Clique <a href="{{ $email_confirmation_url }}">neste link</a> para confirmar seu e-mail.<br />
 Em seguida, faça login e prossiga solicitando isenções de taxa ou efetuando inscrições ou matrículas.<br />
 <br />
 Salientamos que é de responsabilidade do usuário acompanhar o andamento do processo no sistema, desde a validade de seu cadastro básico, até sua(s) eventual(is) inscrição(ões) e resultado da(s) mesma(s).
